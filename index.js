7e03d346e7f51cab7eb560faa53e842e4474d3c1
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-03 23:42:52 +0100

    Ajout d'un middleware pour verifier l'existence des données dans redis

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const client=redis.createClient(REDIS_PORT);
 
 const app=express();
 
+//Set response
+
+function setResponse(username, repos){
+    return `<h2> ${username} has ${repos} github repositories </h2>`;
+}
+
 //Make request to Github for data
 async function getRepos(req, res, next){
     try{
@@ -19,12 +25,6 @@ async function getRepos(req, res, next){
         const data=await response.json();
 
         const repos=data.public_repos;
-
-        //Set response
-
-        function setResponse(username, repos){
-            return `<h2> ${username} has ${repos} github repositories </h2>`;
-        }
 
         //Set data to redis
         client.setex(username,3600,repos);
@@ -37,7 +37,23 @@ async function getRepos(req, res, next){
     }
 }
 
-app.get('/repos/:username', getRepos);
+// Cache middleware
+
+function cache(req, res, next){
+    const {username}=req.params;
+
+    client.get(username, (err, data)=>{
+        if(err) throw err;
+
+        if(data!==null){
+            res.send(setResponse(username, data))
+        }else{
+            next();
+        }
+    })
+}
+
+app.get('/repos/:username',cache, getRepos);
 
 app.listen(5000, ()=>{
     console.log(`App listening on port ${PORT}`);
