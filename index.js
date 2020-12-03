0aab8b6ed0475364017ca7c15461ab27381f50e1
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-03 23:16:42 +0100

    Implemntation of fetch github user data and cache public repos into redis

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ async function getRepos(req, res, next){
     try{
 
         console.log('fetching data....');
-        
+        const {username}=req.params;
+        const response =await fetch(`https://api.github.com/users/${username}`);
+        const data=await response.json();
+
+        const repos=data.public_repos;
+
+        //Set response
+
+        function setResponse(username, repos){
+            return `<h2> ${username} has ${repos} github repositories </h2>`;
+        }
+
+        //Set data to redis
+        client.setex(username,3600,repos);
+
+        res.send(setResponse(username, repos));
 
     }catch(err){
         console.error(err);
